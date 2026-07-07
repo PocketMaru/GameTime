0d8615faa3d7d39a55803463d2676a0131870d76
PocketMaru <joshua.hauer.dev@gmail.com>
2026-07-07 01:37:58 -0400

    Refactor: Update GameTimerFormVM to use TimeComponents and TimeConverter

--- GameTime/Features/GameTimer/ViewModels/GameTimerFormVM.swift
+++ GameTime/Features/GameTimer/ViewModels/GameTimerFormVM.swift
@@ -13,12 +13,20 @@ enum FormMode {
     case edit
 }
 
+/// `GameTimerFormVM` is a form responsible for:
+/// - Creation and edit of new and saved timers
+/// `name` is a display variable for the created/edited timers name
+/// `timer` is a display variable that is converted from string to double.
+/// `mode` determins which action is being performed by the user to determin how the sumbission should be handled.
+/// `validate()` validates user input returning a boolean value.
+/// `onSubmit` is a optional callback defined in `GameTimerVM` where it is saved after create/edit.
 @MainActor
 @Observable
 final class GameTimerFormVM {
     var draft: GameTimer.Draft
     var name: String? = ""
-    var timer: String? = ""
+    var timeComponents = TimeComponents()
+
     var mode: FormMode
     var onSubmit: ((GameTimer.Draft) -> Void)?
     
@@ -27,18 +35,25 @@ final class GameTimerFormVM {
         mode: FormMode,
         onSubmit: ((GameTimer.Draft) -> Void)?
     ) {
-        if mode == .edit {
-            name = draft.name
-            timer = draft.timer.map { String($0) } ?? ""
-        }
         self.draft = draft
         self.mode = mode
         self.onSubmit = onSubmit
+        
+        if mode == .edit {
+            name = draft.name
+            if let seconds = draft.timer {
+                let converted = TimeConverter.convertFromSeconds(seconds)
+                timeComponents.seconds = converted.seconds
+                timeComponents.minutes = converted.minutes
+                timeComponents.hours = converted.hours
+            }
+        }
+        
     }
     
     func submit() {
         draft.name = name
-        draft.timer = Double(timer ?? "0")
+        draft.timer = TimeConverter.convertToSeconds(time: timeComponents)
         guard validate() else { return }
         onSubmit?(draft)
     }
