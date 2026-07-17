44138486001cd6c1c79062e803ae892e2e2d9f78
PocketMaru <joshua.hauer.dev@gmail.com>
2026-07-17 13:59:04 -0400

    chore: Remove redundant VStack in TimerNameView

--- GameTime/Features/Timer/Views/SubViews/TimerNameView.swift
+++ GameTime/Features/Timer/Views/SubViews/TimerNameView.swift
@@ -3,11 +3,9 @@ import SwiftUI
 struct TimerNameView: View {
     @Binding var name: String
     var body: some View {
-        VStack {
-            TextField("Label", text: $name)
-                .foregroundStyle(Color.primaryText)
-                .frame(width: 250)
-                .textFieldStyle()
-        }
+        TextField("Label", text: $name)
+            .foregroundStyle(Color.primaryText)
+            .frame(width: 250)
+            .textFieldStyle()
     }
 }
