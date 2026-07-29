6f7897d4e81cb8f061d4cf4727754cc9f7773753
PocketMaru <joshua.hauer.dev@gmail.com>
2026-07-29 15:26:14 -0400

    refactor: Use ForEach for preset buttons in PresetTimerRowView

--- GameTime/Features/GameTimer/Views/SubViews/RowViews/PresetTimerRowView.swift
+++ GameTime/Features/GameTimer/Views/SubViews/RowViews/PresetTimerRowView.swift
@@ -1,71 +1,31 @@
 import SwiftUI
 
 struct PresetTimerRowView: View {
+    let presets: [TimeComponents] = [
+        .init(minutes: 1),
+        .init(minutes: 2),
+        .init(minutes: 3),
+        .init(minutes: 4),
+        .init(minutes: 5),
+        .init(minutes: 10),
+        .init(minutes: 15),
+        .init(minutes: 20),
+        .init(minutes: 30),
+        .init(minutes: 45),
+        .init(hours: 1),
+        .init(hours: 2)
+    ]
     let action: (TimeComponents) -> Void
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             Section {
                 HStack(spacing: 10) {
-                    PresetButtonView(
-                        value: TimeComponents(seconds: 0, minutes: 1, hours: 0),
-                        title: "MIN",
-                        action: action
-                    )
-                    PresetButtonView(
-                        value: TimeComponents(seconds: 0, minutes: 2, hours: 0),
-                        title: "MIN",
-                        action: action
-                    )
-                    PresetButtonView(
-                        value: TimeComponents(seconds: 0, minutes: 3, hours: 0),
-                        title: "MIN",
-                        action: action
-                    )
-                    PresetButtonView(
-                        value: TimeComponents(seconds: 0, minutes: 4, hours: 0),
-                        title: "MIN",
-                        action: action
-                    )
-                    PresetButtonView(
-                        value: TimeComponents(seconds: 0, minutes: 5, hours: 0),
-                        title: "MIN",
-                        action: action
-                    )
-                    PresetButtonView(
-                        value: TimeComponents(seconds: 0, minutes: 10, hours: 0),
-                        title: "MIN",
-                        action: action
-                    )
-                    PresetButtonView(
-                        value: TimeComponents(seconds: 0, minutes: 15, hours: 0),
-                        title: "MIN",
-                        action: action
-                    )
-                    PresetButtonView(
-                        value: TimeComponents(seconds: 0, minutes: 20, hours: 0),
-                        title: "MIN",
-                        action: action
-                    )
-                    PresetButtonView(
-                        value: TimeComponents(seconds: 0, minutes: 30, hours: 0),
-                        title: "MIN",
-                        action: action
-                    )
-                    PresetButtonView(
-                        value: TimeComponents(seconds: 0, minutes: 45, hours: 0),
-                        title: "MIN",
-                        action: action
-                    )
-                    PresetButtonView(
-                        value: TimeComponents(seconds: 0, minutes: 0, hours: 1),
-                        title: "HR",
-                        action: action
-                    )
-                    PresetButtonView(
-                        value: TimeComponents(seconds: 0, minutes: 0, hours: 2),
-                        title: "HR",
-                        action: action
-                    )
+                    ForEach(presets, id: \.compare) { timer in
+                        PresetButtonView(
+                            value: timer,
+                            action: action
+                        )
+                    }
                 }
             } header: {
                 Text("Presets")
@@ -75,5 +35,11 @@ struct PresetTimerRowView: View {
                     .padding(.horizontal)
             }
         }
+    }
+}
+
+private extension TimeComponents {
+    var compare: String {
+        "\(hours):\(minutes):\(seconds)"
     }
 }
