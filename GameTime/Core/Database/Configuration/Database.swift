e5551ee6f889d5ae3ecbcd66d6f0e33e9059d654
PocketMaru <joshua.hauer.dev@gmail.com>
2026-08-05 20:04:41 -0400

    feat: Add core database configuration and migrations

---
+++ GameTime/Core/Database/Configuration/Database.swift
@@ -0,0 +1,54 @@
+import OSLog
+import SQLiteData
+
+func appDatabase() throws -> any DatabaseWriter {
+    @Dependency(\.context) var context
+    
+    var config = Configuration()
+    
+#if DEBUG
+    config.prepareDatabase { db in
+        db.trace(options: .profile) {
+            if context == .preview {
+                print("\($0.expandedDescription)")
+            } else {
+                logger.debug("\($0.expandedDescription)")
+            }
+        }
+    }
+#endif
+    
+    let db = try defaultDatabase(
+        configuration: config
+    )
+    
+    var migrator = DatabaseMigrator()
+    
+#if DEBUG
+    migrator.eraseDatabaseOnSchemaChange = true
+#endif
+    
+    migrator.registerMigration("Create game timers") { db in
+        try #sql(
+            """
+            CREATE TABLE "gameTimers" (
+                "id" TEXT PRIMARY KEY NOT NULL ON CONFLICT REPLACE DEFAULT (uuid()),
+                "name" TEXT NOT NULL,
+                "timer" INTEGER NOT NULL
+            ) STRICT
+            """
+        )
+        .execute(db)
+    }
+    
+    try migrator.migrate(db)
+    
+    logger.info("Opened database at `\(db.path)`")
+    
+    return db
+}
+
+private let logger = Logger(
+    subsystem: "GameTime",
+    category: "Database"
+)
--- GameTime/Core/EntryPoint/GameTimeApp.swift
+++ GameTime/Core/EntryPoint/GameTimeApp.swift
@@ -1,8 +1,16 @@
 import SwiftUI
+import SQLiteData
 
 @main
 struct GameTimeApp: App {
     @State private var vm = GameTimerVM()
+    
+    init() {
+        prepareDependencies {
+            $0.defaultDatabase = try! appDatabase()
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             GameTimeView(vm: vm)
